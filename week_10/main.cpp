765325acfeca09b07b534dfd97a1b6742329710c
Atanas Ivanov <naskuuu123@gmail.com>
2023-12-19 14:40:57 +0200

    add main.cpp

--- week_10/main.cpp
+++ week_10/main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
 int main() {
-    return 0;
+    int* ptr = new int(5);
+
+    std::cout << ptr << std::endl;
+    std::cout << *ptr << std::endl;
+
+    // This leads to memory leak of the first heap memory alocation!!!
+    ptr = new int(10);
+
+    delete ptr;
 }
