1fa4f3fe0e9d0a3f68ada42556107c10321b0aeb
Ivo <external.ivaylo.stoyanov@kuehne-nagel.com>
2023-12-22 11:32:14 +0200

    some work being done :)

--- week_11/main.cpp
+++ week_11/main.cpp
@@ -1,5 +1,47 @@
 #include <iostream>
 
+void printClientIds(int** hotel, const size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        if (hotel[i] != nullptr) {
+            std::cout << *hotel[i] << " ";
+        }
+    }
+    std::cout << std::endl;
+}
+
+void putClientIdInIndex(int** hotel, const size_t index, const int clientId) {
+    if (hotel[index] == nullptr) {
+        hotel[index] = new int(clientId);
+    }
+}
+
+void clearClientIdInIndex(int** hotel, const size_t index, const int clientId) {
+    if (hotel[index] != nullptr) {
+        delete hotel[index];
+        hotel[index] = nullptr;
+    }
+}
+
 int main() {
-    
+    size_t size = 10;
+    int** hotel = new int*[size]();
+    int clientId = 5;
+    hotel[0] = nullptr;
+    hotel[0] = new int(clientId);
+    hotel[1] = new int(clientId);
+
+    printClientIds(hotel, size);
+
+    hotel[0] = nullptr;
+
+    printClientIds(hotel, size);
+
+    for (size_t i = 0; i < size; i++) {
+        if (hotel[i] != nullptr) {
+            delete hotel[i]; 
+        }
+    }
+    delete [] hotel;
+
+    std::cout << "END";
 }
